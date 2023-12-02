c4eb225ad989bf05e53ac5b6dd42d78dbcb5334d
Ramil spiN <ramil_spin@mail.ru>
2023-12-02 16:18:47 +0300

    Add API Gateway

--- eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.example.eurekaclient.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping("/main")
 public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
 
     @GetMapping("/test")
     public String test() {
-        return "test";
+        log.info("!!test controller - " + instanceId);
+        return instanceId;
     }
 }
